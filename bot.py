f815e8cee041d72ef85e34a519c4bf1ce41efd2b
rajdev9101 <devraj93957@gmail.com>
2025-07-17 22:48:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from flask import Flask
 import json, os, logging
 
-TOKEN = "7777252416:AAGG07twWDJjfFvldXqxaxrJmAFXa0yQAbA"
+TOKEN = "7777252416:7777252416:AAFtLx4YhHRFrBkKWmn-Gk6ZQgaPEiP-7bQ"
 PORT = 8080
 
 app = Flask(__name__)
